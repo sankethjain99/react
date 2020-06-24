be9230a41a7966342ade4e5c9ef39a332ba7937e
sanketh <jsanketh1799@gmail.com>
2020-06-25 00:38:46 +0530

    components-2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -2,39 +2,38 @@ import React , {Component} from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody,
     CardTitle } from 'reactstrap';
 
+class Menu extends Component {
 
-    class Menu extends Component {
+    constructor(props) {
+        super(props);
 
-        constructor(props) {
-            super(props);
-    
-            this.state = {
-                selectedDish: null
-            }
+        this.state = {
+            selectedDish: null
         }
-        onDishSelect(dish) {
-            this.setState({ selectedDish: dish});
-        }
-    
-        renderDish(dish) {
-            if (dish != null)
-                return(
-                    <Card>
-                        <CardImg top src={dish.image} alt={dish.name} />
-                        <CardBody>
-                          <CardTitle>{dish.name}</CardTitle>
-                          <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
-                );
-            else
-                return(
-                    <div></div>
-                );
-        }
+    }
 
-    render(){
+    onDishSelect(dish) {
+        this.setState({ selectedDish: dish});
+    }
 
+    renderDish(dish) {
+        if (dish != null)
+            return(
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        else
+            return(
+                <div></div>
+            );
+    }
+
+    render() {
         const menu = this.props.dishes.map((dish) => {
             return (
               <div  className="col-12 col-md-5 m-1">
